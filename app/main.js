c2535d496d240ec369e4b28af09f62f1cd5e27d9
it62070180 <56427172+it62070180@users.noreply.github.com>
2021-10-04 20:49:27 +0700

    Change button to Touch
    เปลี่ยน button เป็น TouchableOpacity ใน
    mainJs แล้วเพิ่ม navigate นิดหน่อย

--- app/main.js
+++ app/main.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { database } from './database/db';
-import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-function Screen() {
+function Screen({navigation}) {
     const [Deck, setDeck] = useState([]);
     const [Name, setName] = useState("")
     const [UID, setUID] = useState("");
@@ -23,17 +23,74 @@ function Screen() {
     }, [])
   
     return (
-      <SafeAreaView>
-        <View>
+      <SafeAreaView style={styles.container} >
           <Text>
             {UID}
           </Text>
-          <Button title='PLAY' />
-          <Button title='DECK' />
-          <Button title='EXIT' />
-        </View>      
+            <View style={styles.viewbutton}>
+              <TouchableOpacity style={styles.buttonPlay}
+              // onPress={() => navigation.navigate('Play')}
+              > 
+              <Text style={styles.textinbutton}> Play </Text> 
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.buttonDeck}
+              onPress={() => navigation.navigate('Deck')}> 
+              <Text style={styles.textinbutton}> Deck </Text> 
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.buttonExit}
+               onPress={() => navigation.navigate('LogIn')}> 
+              <Text style={styles.textinbutton}> Exit </Text> 
+              </TouchableOpacity>
+            </View> 
       </SafeAreaView>
     );
 }
+
+
+const styles = StyleSheet.create({
+  // SafeAreaView
+  container: {
+    flex: 1,
+  },
+    viewbutton:{
+      flex:1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    // ปุ่ม Play
+    buttonPlay:{
+      borderWidth:3,
+      borderColor:'#0086f5',
+      backgroundColor:'#4286f5',
+      borderRadius:2,
+      margin:10,
+      paddingHorizontal: 30,
+      paddingVertical: 5
+    },
+    // ปุ่ม Deck 
+    buttonDeck:{
+      borderWidth:3,
+      borderColor:'#40D60E',
+      backgroundColor:'#84D60E',
+      borderRadius:2,
+      margin:10,
+      paddingHorizontal: 30,
+      paddingVertical: 5
+    },
+    // ปุ่ม Exit 
+    buttonExit:{
+      borderWidth:3,
+      borderColor:'#A00E0E',
+      backgroundColor:'#D60E0E',
+      borderRadius:2,
+      margin:10,
+      paddingHorizontal: 30,
+      paddingVertical: 5
+    },
+    textinbutton:{
+      color:"black",
+      fontWeight:'bold'
+    }
+})
 
 export default Screen;
